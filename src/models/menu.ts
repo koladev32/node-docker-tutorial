77fa9ce1530eb86c999106c8c0b16f6365560455
Kolawole Mangabo <onaelmangabo@gmail.com>
2021-10-17 13:49:59 +0100

    - feat: add menu model

---
+++ src/models/menu.ts
@@ -0,0 +1,24 @@
+import { IMenu } from "../types/menu";
+import {model, Schema} from "mongoose";
+
+const menuSchema: Schema = new Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: String,
+        required: true
+    },
+    created: {
+        type: Date,
+        default: Date.now
+    }
+
+}, {timestamps: true})
+
+export default model<IMenu>("Menu", menuSchema);
--- src/types/menu.ts
+++ src/types/menu.ts
@@ -2,8 +2,6 @@ import { Document } from "mongoose";
 
 export interface IMenu extends Document{
     name: string
-    restaurant: string;
     description: string;
     price: number;
-    created: Date;
 }
