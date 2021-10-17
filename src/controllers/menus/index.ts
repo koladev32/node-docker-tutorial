589d142c74ce1aa9d5bdea25f986849eacb59d86
Kolawole Mangabo <onaelmangabo@gmail.com>
2021-10-17 15:40:13 +0100

    - feat: add delete and retrieve endpoint

--- src/controllers/menus/index.ts
+++ src/controllers/menus/index.ts
@@ -11,6 +11,17 @@ const getMenus = async (req: Request, res: Response): Promise<void> => {
     }
 }
 
+const retrieveMenu = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const { params: { id }} = req;
+        const menu: IMenu | null = await Menu.findById({_id: id});
+
+        res.status(menu ? 200 : 404).json({menu});
+    } catch (error) {
+        throw error;
+    }
+}
+
 const addMenu = async (req: Request, res: Response): Promise<void> => {
     try {
         const body = req.body as Pick<IMenu, "name" | "description" | "price">;
@@ -41,7 +52,7 @@ const updateMenu = async (req: Request, res: Response): Promise<void> => {
             {_id: id}, body
         );
 
-        res.status(200).json({
+        res.status(updateMenu? 200 : 404).json({
             menu: updateMenu
         })
     } catch (error) {
@@ -49,4 +60,16 @@ const updateMenu = async (req: Request, res: Response): Promise<void> => {
     }
 }
 
-export {getMenus, addMenu, updateMenu};
+const deleteMenu = async (req: Request, res: Response): Promise<void> => {
+    try {
+        const deletedMenu: IMenu | null = await Menu.findByIdAndRemove(
+            req.params.id
+        )
+        res.status(204).json({
+            todo: deletedMenu
+        })
+    } catch (error) {
+        throw error
+    }
+}
+export {getMenus, addMenu, updateMenu, deleteMenu, retrieveMenu};
